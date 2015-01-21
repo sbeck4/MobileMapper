68fafd6787ffef465aee40fb7a6fadd6a63de395
Shannon Beck <Shannon.e.beck@gmail.com>
2015-01-21 09:52:07 -0800

    first commit

--- MobileMapper/ViewController.m
+++ MobileMapper/ViewController.m
@@ -7,21 +7,80 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property MKPointAnnotation *mobileMakersAnnotation;
+@property CLLocationManager *locationManager;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(37.790752, -122.402071);
+    self.mobileMakersAnnotation = [MKPointAnnotation new];
+    self.mobileMakersAnnotation.coordinate = coordinate;
+    self.mobileMakersAnnotation.title = @"Mobile Makers";
+    [self.mapView addAnnotation:self.mobileMakersAnnotation];
+
+    NSString *address = @"Mount Rushmore";
+    NSString *address2 = @"Fountain Inn, SC";
+
+    [self geocoder:address];
+    [self geocoder:address2];
+
+    self.locationManager = [CLLocationManager new];
+    [self.locationManager requestWhenInUseAuthorization];
+    self.mapView.showsUserLocation = YES;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)geocoder:(NSString *)address
+{
+    CLGeocoder *geocoder = [CLGeocoder new];
+
+    [geocoder geocodeAddressString:address completionHandler:^(NSArray *placemarks, NSError *error)
+     {
+         for (CLPlacemark *placemark in placemarks)
+         {
+             MKPointAnnotation *annotation = [MKPointAnnotation new];
+             annotation.coordinate = placemark.location.coordinate;
+             annotation.title = placemark.name;
+             [self.mapView addAnnotation:annotation];
+         }
+     }];
 }
+
+
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    CLLocationCoordinate2D centerCoordinate = view.annotation.coordinate;
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.01, 0.01);
+    MKCoordinateRegion region = MKCoordinateRegionMake(centerCoordinate, span);
+    [self.mapView setRegion:region animated:YES];
+}
+
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    if (annotation == self.mobileMakersAnnotation)
+    {
+        MKPinAnnotationView *pin = [[MKPinAnnotationView alloc]initWithAnnotation:annotation reuseIdentifier:nil];
+        pin.image = [UIImage imageNamed:@"mobilemakers.png"];
+        pin.canShowCallout = YES;
+        pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        return pin;
+    }
+    else
+    {
+        return nil;
+    }
+}
+
 
 @end
